2eba780e5d34219666fcd3536747f9415f30540b
Jan Halamka <Jan.Halamka@saint-gobain.com>
2021-11-22 11:40:02 +0100

    soucetPismen

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,8 +6,25 @@ var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "sev
 
 var result = strings.Select(x => x.ToUpper());
 
-PrintList(result.ToList());
+var vypisCisla = numbers.Select(x => strings[x]);
 
+bool result2 = numbers.All(x => x % 2 == 0);
+
+var soucetPismen = strings.Select(x => x.Length).Sum();
+Console.WriteLine($"vsechna slova v poli strings maji dohromady {soucetPismen} pismen");
+
+Console.WriteLine($"jsou vsechna cisla suda: {result2}");
+
+//PrintList(result.ToList());
+PrintList(vypisCisla.ToList());
+
+//3 vypiste cisla v poli numbers jako slova
+
+//Václavův výpis
+foreach (var item in numbers)
+{
+    Console.WriteLine("vaclav: " + strings[item]);
+}
 
 static void PrintList(List<string> listToPrint)
 {
