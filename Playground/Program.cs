b7cbc9e09da1fd3256e4362f34343669b978ff79
Jan Halamka <Jan.Halamka@saint-gobain.com>
2021-11-22 10:54:47 +0100

    metoda na tisk listu

---
+++ Playground/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+var result = strings.Select(x => x.ToUpper());
+
+PrintList(result.ToList());
+
+
+static void PrintList(List<string> listToPrint)
+{
+
+    foreach (var item in listToPrint)
+    {
+        Console.WriteLine(item);
+    }
+}
+
